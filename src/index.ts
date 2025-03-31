056f428e951f5cca77770d4396c1ed7f4395e932
Mike Valeriano <mkvlrn@proton.me>
2025-03-31 14:02:28 -0300

    chore: proper npm publish

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,24 @@
  */
 
 /**
+ * Creates a successful Result with the given value.
+ * @param value The success value
+ * @returns A Result object representing success
+ */
+function success<T>(value: T): Result<T, never> {
+  return { ok: true, value: value };
+}
+
+/**
+ * Creates a successful Result with the given value.
+ * @param value The success value
+ * @returns A Result object representing success
+ */
+function error<E = Error>(error: E): Result<never, E> {
+  return { ok: false, error: error };
+}
+
+/**
  * Result type to represent the outcome of an operation.
  * It can either be a success with a value or an error with an error message.
  * This is a generic type that can be used with any type of value and error.
@@ -19,21 +37,6 @@ export const Result: {
   success<T>(value: T): Result<T, never>;
   error<E = Error>(error: E): Result<never, E>;
 } = {
-  /**
-   * Creates a successful Result with the given value.
-   * @param value The success value
-   * @returns A Result object representing success
-   */
-  success<T>(value: T): Result<T, never> {
-    return { ok: true, value };
-  },
-
-  /**
-   * Creates an error Result with the given error.
-   * @param error The error value
-   * @returns A Result object representing failure
-   */
-  error<E = Error>(error: E): Result<never, E> {
-    return { ok: false, error };
-  },
+  success: success,
+  error: error,
 };
