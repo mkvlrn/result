0a3be8cd388b5bf2e69d9f8084dd3f35f0336e1f
Mike Valeriano <mkvlrn@proton.me>
2025-05-11 16:16:50 -0300

    fix: rework object

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,4 @@
 /**
- * This module exports a Result type and helper functions that represent the outcome of an operation.
- * @module
- */
-
-/**
- * Creates a successful Result with the given value.
- * @param value The success value
- * @returns A Result object representing success
- */
-function success<T>(value: T): Result<T, never> {
-  return { ok: true, value: value };
-}
-
-/**
- * Creates a successful Result with the given value.
- * @param value The success value
- * @returns A Result object representing success
- */
-function error<E = Error>(error: E): Result<never, E> {
-  return { ok: false, error: error };
-}
-
-/**
  * Result type to represent the outcome of an operation.
  * It can either be a success with a value or an error with an error message.
  * This is a generic type that can be used with any type of value and error.
@@ -37,6 +14,21 @@ export const Result: {
   success<T>(value: T): Result<T, never>;
   error<E = Error>(error: E): Result<never, E>;
 } = {
-  success: success,
-  error: error,
+  /**
+   * Creates a successful Result with the given value.
+   * @param value The success value
+   * @returns A Result object representing success
+   */
+  success: function <T>(value: T): Result<T, never> {
+    return { ok: true, value: value };
+  },
+
+  /**
+   * Creates a successful Result with the given value.
+   * @param value The success value
+   * @returns A Result object representing success
+   */
+  error: function <E = Error>(error: E): Result<never, E> {
+    return { ok: false, error: error };
+  },
 };
