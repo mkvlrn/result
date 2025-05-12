f3d0197f08a65a4ccb7ef6370760c15168bc035c
Mike Valeriano <mkvlrn@proton.me>
2025-05-12 06:17:07 -0300

    fix: adjust exports documentation again

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,10 @@
 /**
- * Creates a successful Result with the given value.
- * @param value The success value
- * @returns A Result object representing success
- */
-function resultSuccess<T>(value: T): Result<T, never> {
-  return { ok: true, value: value };
-}
-
-/**
- * Creates a successful Result with the given value.
- * @param value The success value
- * @returns A Result object representing success
- */
-function resultError<E = Error>(error: E): Result<never, E> {
-  return { ok: false, error: error };
-}
-
-/**
  * Result type to represent the outcome of an operation.
  * It can either be a success with a value or an error with an error message.
  * This is a generic type that can be used with any type of value and error.
  *
- * It also aliases the success and error functions to make it easier to create
- * Result objects.
+ * It is also an alias object containing the success and error functions to
+ * make it easier to create Result objects.
  */
 export type Result<T, E = Error> =
   | { readonly ok: true; readonly value: T }
@@ -33,6 +15,16 @@ export const Result: {
   success<T>(value: T): Result<T, never>;
   error<E = Error>(error: E): Result<never, E>;
 } = {
-  success: resultSuccess,
-  error: resultError,
+  /**
+   * Creates a successful Result with the given value.
+   * @param value The success value
+   * @returns A Result object representing success
+   */
+  success: <T>(value: T): Result<T, never> => ({ ok: true, value: value }),
+  /**
+   * Creates an error Result with the given error.
+   * @param error The error value
+   * @returns A Result object representing error
+   */
+  error: <E = Error>(error: E): Result<never, E> => ({ ok: false, error: error }),
 };
